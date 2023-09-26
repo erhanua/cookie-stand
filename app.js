309a6ec14e637de7956b1d3637e854c890917584
Adınız Soyadınız <email@adresiniz.com>
2023-09-26 23:02:17 +0100

    changes

--- app.js
+++ app.js
@@ -15,179 +15,42 @@ const hours = [
   "7pm",
 ];
 
-// give a random number between two numbers
+function Shop(location, minCust, maxCust, avgCookiesPerCust) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookiesPerCust = avgCookiesPerCust;
+  this.customersPerHour = [];
+  this.cookiesPerHour = [];
+  this.totalCookieSold = 0;
+}
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// create our first shop
-const seattle = {
-  location: "Seattle",
-  minCust: 23,
-  maxCust: 65,
-  avgCookiesPerCust: 6.3,
-  customersPerHour: [],
-  cookiesPerHour: [],
-  totalCookieSold: 0,
-  calculateSales: function () {
-    for (let i = 0; i < hours.length; i++) {
-      // get the number of customers for this hour
-      const hourCustomers = randomNumber(this.minCust, this.maxCust);
-      this.customersPerHour.push(hourCustomers);
-
-      // get the number of cookies sold this hour
-      const hourCookiesSold = Math.floor(
-        hourCustomers * this.avgCookiesPerCust
-      );
-      this.cookiesPerHour.push(hourCookiesSold);
-
-      // increase the total cookies by adding this hours sales to it
-      this.totalCookieSold = this.totalCookieSold + hourCookiesSold;
-      console.log(this.totalCookieSold);
-    }
-  },
-};
-
-const tokyo = {
-  location: "Tokyo",
-  minCust: 3,
-  maxCust: 24,
-  avgCookiesPerCust: 1.2,
-  customersPerHour: [],
-  cookiesPerHour: [],
-  totalCookieSold: 0,
-  calculateSales: function () {
-    for (let i = 0; i < hours.length; i++) {
-      // get the number of customers for this hour
-      const hourCustomers = randomNumber(this.minCust, this.maxCust);
-      this.customersPerHour.push(hourCustomers);
-
-      // get the number of cookies sold this hour
-      const hourCookiesSold = Math.floor(
-        hourCustomers * this.avgCookiesPerCust
-      );
-      this.cookiesPerHour.push(hourCookiesSold);
-
-      // increase the total cookies by adding this hours sales to it
-      this.totalCookieSold = this.totalCookieSold + hourCookiesSold;
-    }
-  },
-};
-
-seattle.calculateSales();
-tokyo.calculateSales();
-
-// get the element on the page with the id salesData
-const salesData = document.getElementById("salesData");
-
-// add the title for the location
-const seattleH2 = document.createElement("h2");
-seattleH2.textContent = seattle.location;
-salesData.appendChild(seattleH2);
-
-// create a list to show the cookies sold at each hour
-const seattleUl = document.createElement("ul");
-// loop through out data and for each item create an <li>
-for (let i = 0; i < hours.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = `${hours[i]}: ${seattle.cookiesPerHour[i]} cookies`;
-  seattleUl.appendChild(li);
-}
-
-salesData.appendChild(seattleUl);
-
-// add the title for the location
-const tokyoH2 = document.createElement("h2");
-tokyoH2.textContent = tokyo.location;
-salesData.appendChild(tokyoH2);
-
-// create a list to show the cookies sold at each hour
-const tokyoUl = document.createElement("ul");
-// loop through out data and for each item create an <li>
-for (let i = 0; i < hours.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = `${hours[i]}: ${tokyo.cookiesPerHour[i]} cookies`;
-  tokyoUl.appendChild(li);
-}
-
-salesData.appendChild(tokyoUl);
-
-// ... [Önceki kodlarınız burada]
-
-const dubai = {
-  location: "Dubai",
-  minCust: 11,
-  maxCust: 38,
-  avgCookiesPerCust: 3.7,
-  customersPerHour: [],
-  cookiesPerHour: [],
-  totalCookieSold: 0,
-  calculateSales: function () {
-    for (let i = 0; i < hours.length; i++) {
-      const hourCustomers = randomNumber(this.minCust, this.maxCust);
-      this.customersPerHour.push(hourCustomers);
-      const hourCookiesSold = Math.floor(
-        hourCustomers * this.avgCookiesPerCust
-      );
-      this.cookiesPerHour.push(hourCookiesSold);
-      this.totalCookieSold += hourCookiesSold;
-    }
-  },
-};
-
-const paris = {
-  location: "Paris",
-  minCust: 20,
-  maxCust: 38,
-  avgCookiesPerCust: 2.3,
-  customersPerHour: [],
-  cookiesPerHour: [],
-  totalCookieSold: 0,
-  calculateSales: function () {
-    for (let i = 0; i < hours.length; i++) {
-      const hourCustomers = randomNumber(this.minCust, this.maxCust);
-      this.customersPerHour.push(hourCustomers);
-      const hourCookiesSold = Math.floor(
-        hourCustomers * this.avgCookiesPerCust
-      );
-      this.cookiesPerHour.push(hourCookiesSold);
-      this.totalCookieSold += hourCookiesSold;
-    }
-  },
-};
-
-const lima = {
-  location: "Lima",
-  minCust: 2,
-  maxCust: 16,
-  avgCookiesPerCust: 4.6,
-  customersPerHour: [],
-  cookiesPerHour: [],
-  totalCookieSold: 0,
-  calculateSales: function () {
-    for (let i = 0; i < hours.length; i++) {
-      const hourCustomers = randomNumber(this.minCust, this.maxCust);
-      this.customersPerHour.push(hourCustomers);
-      const hourCookiesSold = Math.floor(
-        hourCustomers * this.avgCookiesPerCust
-      );
-      this.cookiesPerHour.push(hourCookiesSold);
-      this.totalCookieSold += hourCookiesSold;
-    }
-  },
-};
-
-// Calculate sales and show results on web page
-[dubai, paris, lima].forEach((shop) => {
-  shop.calculateSales();
-  const h2 = document.createElement("h2");
-  h2.textContent = shop.location;
-  salesData.appendChild(h2);
-  const ul = document.createElement("ul");
+Shop.prototype.render = function () {
   for (let i = 0; i < hours.length; i++) {
-    const li = document.createElement("li");
-    li.textContent = `${hours[i]}: ${shop.cookiesPerHour[i]} cookies`;
-    ul.appendChild(li);
+    const hourCustomers = randomNumber(this.minCust, this.maxCust);
+    this.customersPerHour.push(hourCustomers);
+    const hourCookiesSold = Math.floor(hourCustomers * this.avgCookiesPerCust);
+    this.cookiesPerHour.push(hourCookiesSold);
+    this.totalCookieSold += hourCookiesSold;
   }
-  salesData.appendChild(ul);
-});
+};
+
+const seattle = new Shop("Seattle", 23, 65, 6.3);
+
+seattle.render();
+
+const tokyo = new Shop("Tokyo", 3, 24, 1.2);
+tokyo.render();
+
+const dubai = new Shop("Dubai", 11, 38, 3.7);
+dubai.render();
+
+const paris = new Shop("Paris", 20, 38, 2.3);
+paris.render();
+
+const lima = new Shop("Lima", 2, 16, 4.6);
+lima.render();
